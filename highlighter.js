32505c37bf9c40e544421ac9067bfb9b16c4e68a
sanyamkamat <sanyamkamat@gmail.com>
2014-11-12 01:58:13 +0530

    adding setTimeout

--- highlighter.js
+++ highlighter.js
@@ -11,8 +11,15 @@ String.prototype.highlighter = function () {
     $.fn.highlighter = function () {
 //        return this;
         var content = this.text();
-        var dataArray = content.split();
-
-        element.name = element.name.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + that.params.searchString + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<span>$1</span>");
+        var dataArray = content.split(" ");
+        test = "";
+        for(var i=0;i<dataArray.length;i++){
+            setTimeout(function(){
+                this.html = "<span style='color: red'>"+test + dataArray[i] +"</span>" + dataArray.slice(i).join(" ");
+            },200);
+            test+=dataArray[i];
+        }
+        return this;
+//        element.name = element.name.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + that.params.searchString + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<span>$1</span>");
     };
 })(jQuery);
